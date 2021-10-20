7705955ebcd639ee1fb11f989652d1196b0c9e36
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-10-20 23:29:12 +0530

    Navbar changes

--- frontend/src/Components/NavBar/Button/Button.js
+++ frontend/src/Components/NavBar/Button/Button.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Button = () => {
   return (
     <Link to='sign-up'>
-      <button className='btn'>Sign Up</button>
+      <button className='btn'>Join us </button>
     </Link>
   );
 };
