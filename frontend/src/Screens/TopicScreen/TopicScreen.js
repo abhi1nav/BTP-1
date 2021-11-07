2c6f9ae8f121dd30b933bb79c10dd2f7221f2536
Kashish Choubey <kashishchoubey7516@gmail.com>
2021-11-08 01:37:05 +0530

    Part-1 Updates 1.2

--- frontend/src/Screens/TopicScreen/TopicScreen.js
+++ frontend/src/Screens/TopicScreen/TopicScreen.js
@@ -1,56 +1,53 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Button from "../../Components/Button/Button";
 import "./TopicScreen.css";
 
 const TopicScreen = () => {
   return (
-    <div className='div-row'>
-      <div>
-        <img
-          className='img-p1'
-          src='https://the-gadgeteer.com/wp-content/uploads/2019/11/pampers-lumi-smart-diapers-01.jpg'
-          alt='lol'
-        ></img>
+    <div className='flexbox'>
+      <div className='flexbox-col'>
+        <div className='left-child'>
+          <img
+            className='img-p1'
+            src='https://the-gadgeteer.com/wp-content/uploads/2019/11/pampers-lumi-smart-diapers-01.jpg'
+            alt='lol'
+          ></img>
+        </div>
 
-        <h2 className='img-blw-txt'>
-          A new smart diaper can send alert to parents when it's time for a
-          change and can also predict the next pee time.
-        </h2>
+        <div className='left-bottom-child'>
+          <h2 className='btn-abv-text'>
+            A new smart diaper can send alert to parents when it's time for a
+            change and can also predict the next pee time.
+          </h2>
+        </div>
 
-        <h2 className='btn-abv-text'>You can download our application here</h2>
+        <div className='left-bottom-child'>
+          <h2 className='btn-abv-text'>
+            You can download our application here
+          </h2>
 
-        <a href='https://www.google.com/'>
-          <Button className='btn-link'>Visit Google</Button>
-        </a>
+          <br />
+          <a href='https://www.google.com/'>
+            <Button className='btn-link'>Visit Google</Button>
+          </a>
+        </div>
       </div>
 
-      <div>
-        <h1 className='h1-top'>
-          A wet diaper is a main cause for rashes and various other bacterial
-          skin infections .{/* infections, loreum ipsum smssdsf sadafdaf. */}
-        </h1>
-
-        <h1 className='h1-top'>
-          A new smart diaper deployed with RFID sensors for real time
-          communication, can sense the wetness and wirelessly send notification
-          to parents/caregiver that it's time for a change , which ultimately
-          cut down on diaper rash and other problems that causes due to late
-          changing.
-        </h1>
-
-        <h1 className='h1-top'>
-          Our application can also predict the next pee time as well which is
-          not just used for babies, but for the patients who are unable to take
-          care of themselves .<br />
-        </h1>
-
-        <h1 className='h1-top'>
-          It could be convenient for a caregiver to be notified that a patient
-          needs changing .
-        </h1>
-
-        <br />
+      <div className='right-child'>
+        <div className='right-child-text'>
+          A new smart diaper can send alert to parents when it's time for a
+          change and can also predict the next pee time.
+          <br />A wet diaper is a main cause for rashes and some urinary
+          infections. <br />A new smart diaper deployed with RFID sensors for
+          real time communication, can sense the wetness and wirelessly send
+          notification to parents/caregiver that it's time for a change, which
+          ultimately cut down on diaper rash and other problems that causes due
+          to late changing.
+          <br /> Our application can also predict the next pee time as well
+          which is not just used for babies, but for the patients who are unable
+          to take care of themselves. It could be convenient for a caregiver to
+          be notified that a patient needs changing.
+        </div>
       </div>
     </div>
   );
